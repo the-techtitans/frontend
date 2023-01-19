dbdf247e87c9d085db52d531c128989319fb0360
Bhavay Goel <goelbhavay@gmail.com>
2023-01-19 10:54:03 +0530

    Add login page

---
+++ login.js
@@ -0,0 +1,28 @@
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    const data = { email: email, password: password };
+  
+    fetch("http://127.0.0.1:3000/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Incorrect email or password");
+        }
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        // redirect to the home page or show a success message
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // show an error message
+      });
+  }
+  
