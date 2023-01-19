ca81656e1adc796938aba9a9b91bfb1e83917b4c
Saksham Mittal <gotlouemail@gmail.com>
2023-01-19 11:05:23 +0530

    Don't submit login form and make API request right

--- login.js
+++ login.js
@@ -1,9 +1,10 @@
-function login() {
+function login(event) {
+    event.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-  
+
     const data = { email: email, password: password };
-  
+
     fetch("http://127.0.0.1:3000/login", {
       method: "POST",
       body: JSON.stringify(data),
@@ -25,4 +26,4 @@ function login() {
         // show an error message
       });
   }
-  
+
