70de52c8ebcba91eaec15fd479bffbf55c1725c0
Saksham Mittal <gotlouemail@gmail.com>
2023-01-20 00:50:47 +0530

    Prevent naming conflict

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 //decide what to render and when to render it
-let jwt = localStorage.getItem('jwt');
-if (jwt) {
+let ijwt = localStorage.getItem('jwt');
+if (ijwt) {
   //login present, show profile page
   document.getElementById('login').className= "hidden";
   document.getElementById('profile').className = "";
