1131def71b8115f928df3a5f3aac1a436e51f147
Saksham Mittal <gotlouemail@gmail.com>
2023-01-20 11:14:56 +0530

    Make sign up button in navbar disappear when logged in

--- navbar.js
+++ navbar.js
@@ -3,5 +3,7 @@ let ijwt = localStorage.getItem('jwt');
 if (ijwt) {
   //login present, show profile page
   document.getElementById('login').className= "hidden";
+  document.getElementById('signup').className= "hidden";
   document.getElementById('profile').className = "";
+  document.getElementById('myapps').className = "";
 }
